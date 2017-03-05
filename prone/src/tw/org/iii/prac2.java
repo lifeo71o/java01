96efb6a3668eff00e4a120b80caa0201bf0012b6
Anchen0214 <paiyu0214@gmail.com>
2017-03-05 14:25:58 +0800

    VAR

---
+++ prone/src/tw/org/iii/prac2.java
@@ -0,0 +1,13 @@
+package tw.org.iii;
+
+public class prac2 {
+
+	public static void main(String[] args) {
+		//[A-Za-z$_] [a-zA-Z0-9$_]
+		//big5=>2^16=65536;UTF8+2^24
+		byte AAA=12;
+		System.out.println(AAA);
+
+	}
+
+}
