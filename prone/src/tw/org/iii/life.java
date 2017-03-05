59d3849e9465e29f4180d478f88dbabb3966b8f8
Anchen0214 <paiyu0214@gmail.com>
2017-03-05 13:21:28 +0800

    2017030501

---
+++ prone/src/tw/org/iii/life.java
@@ -0,0 +1,14 @@
+package tw.org.iii;
+
+public class life {
+
+	public static void main(String[] life) {
+
+	}
+
+	void kk(){
+	//TODO
+	}
+	
+	
+}
---
+++ prone/src/tw/org/iii/world.java
@@ -0,0 +1,10 @@
+package tw.org.iii;
+
+public class world {
+public static void main(String[] life)//String¦r¦ê[]°}¦Clife¦W¤l
+{
+	
+	System.out.println("hello world");
+	
+}
+}
